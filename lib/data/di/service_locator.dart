c3ff068beb5f11aecdd2cb19597bd1959611f451
Daniel Brandão <daniel.brandao.dev@gmail.com>
2025-08-14 18:37:00 -0300

    feat(serviceLocator) added instances

--- lib/data/di/service_locator.dart
+++ lib/data/di/service_locator.dart
@@ -2,6 +2,8 @@ import 'package:app_receitas/data/repositories/recipe_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../ui/auth/auth_viewmodel.dart';
+import '../../ui/favorite_recipes/fav_recipes_viewmodel.dart';
 import '../../ui/recipes/recipe_view_model.dart';
 import '../services/recipe_service.dart';
 
@@ -14,5 +16,9 @@ class ServiceLocator {
     getIt.registerLazySingleton<RecipeService>(() => RecipeService());
     getIt.registerLazySingleton<RecipeRepository>(() => RecipeRepository());
     getIt.registerLazySingleton<RecipeViewModel>(() => RecipeViewModel());
+    getIt.registerLazySingleton<AuthViewModel>(() => AuthViewModel());
+    getIt.registerLazySingleton<FavRecipesViewModel>(
+      () => FavRecipesViewModel(),
+    );
   }
 }
