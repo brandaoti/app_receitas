b477e5135b6ee18b85b63622e41196fae7c1351b
Daniel Brandão <daniel.brandao.dev@gmail.com>
2025-08-14 18:38:45 -0300

    feat(instance repository)

--- lib/data/repositories/recipe_repository.dart
+++ lib/data/repositories/recipe_repository.dart
@@ -14,4 +14,25 @@ class RecipeRepository {
       throw Exception('Failed to load recipes: $e');
     }
   }
+
+  Future<Recipe?> getRecipeById(String id) async {
+    final rawData = await _recipeService.fetchRecipeById(id);
+    return rawData != null ? Recipe.fromMap(rawData) : null;
+  }
+
+  Future<List<Recipe>> getFavRecipes(String userId) async {
+    final rawData = await _recipeService.fetchFavRecipes(userId);
+    return rawData
+        .where((data) => data['recipes'] != null)
+        .map((data) => Recipe.fromMap(data['recipes'] as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> insertFavRecipe(String recipeId, String userId) async {
+    await _recipeService.insertFavRecipe(recipeId, userId);
+  }
+
+  Future<void> deleteFavRecipe(String recipeId, String userId) async {
+    await _recipeService.deleteFavRecipe(recipeId, userId);
+  }
 }
